42d96acd797a6d7bcff8c330a9bab38bd58d2e74
wongaugustine02 <72580849+wongaugustine02@users.noreply.github.com>
2022-02-05 09:39:53 -0500

    Initializations

---
+++ script.js
@@ -0,0 +1,83 @@
+let w1 = "About the Professor\r\nAbout the course\r\nInstallation of a FTP client and accessing the DCA server";
+
+let w2="Basic developer tools setup, e.g. code editor\r\nWeb Caching\r\nCommon desktop applications such as Mac Finder\r\nMail account setup in various apps/devices: desktop, phone, etc";
+
+let w3="Browser tools: console, debugging, etc (Chrome)\r\nCommon best practices: organization of root folder vs working folder, naming\r\nconventions, routine backup/restore, etc";
+
+let w4="Source version control system and best practices\r\nSource repository and access, e.g. git and github";
+
+let w5="Internet and service providers, eg. TLD, Domain Names and Sub-domains\r\n- ISP, HSP\r\n- IP addresses";
+
+let w6="Common protocols used in web technologies (SFTP, HTTP, HTTPS)\r\nMail and it's protocols (SMTP, IMAP)";
+
+let w7="Web site development and move to production processes\r\nBasic site administrations";
+
+let w8="Media file formats and when to use them\r\nCODECS\r\nCompression considerations\r\nImage compression using tinypng.com";
+
+let w9="Markup and stylesheet languages (HTML & CSS)\r\nScripting languages (JavaScript)\r\nProgramming languages";
+
+let w10="Intro to Libraries (e.g. jQuery) and frameworks (React, Vue.js), JavaScript\r\nHTML, CSS and Javascript FW+tools";
+
+let w11="Collaboration Workflow  (e.g. Proposal phase)\r\nCollaboration Technologies (e.g. Google Doc)";
+
+let w12="File exchange formats (e.g. XML, JSON)\r\nWeb APIs\r\nWeb services (e.g. MapQuest)";
+
+let w13="Final term test\r\nAnswer True/false MC and/or fill-in-bank questions";
+
+document.getElementById("path").onclick = function () {
+    //insert console.log(your dca username);
+    //e.g., console.log("augustw4");
+}
+
+document.getElementById("r0c0").onclick = function () {
+   alert(w1);
+}
+
+document.getElementById("r0c1").onclick = function () {
+   alert(w2);
+}
+
+document.getElementById("r0c2").onclick = function () {
+   alert(w3);
+}
+
+document.getElementById("r0c3").onclick = function () {
+   alert(w4);
+}
+
+document.getElementById("r0c4").onclick = function () {
+   alert(w5);
+}
+
+document.getElementById("r1c0").onclick = function () {
+   alert(w6);
+}
+
+document.getElementById("r1c1").onclick = function () {
+   alert(w7);
+}
+
+document.getElementById("r1c2").onclick = function () {
+   alert(w8);
+}
+
+document.getElementById("r1c3").onclick = function () {
+   alert(w9);
+}
+
+document.getElementById("r1c4").onclick = function () {
+   alert(w10);
+}
+
+
+document.getElementById("r2c0").onclick = function () {
+   alert(w11);
+}
+
+document.getElementById("r2c1").onclick = function () {
+   alert(w12);
+}
+
+document.getElementById("r2c2").onclick = function () {
+   alert(w13);
+}
